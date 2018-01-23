e7a1c04d9f55e042342d1c5003973faa5f37a79c
Andrazh <andraz.hajdenkumer@gmail.com>
2018-01-23 23:29:50 +0100

    exams

---
+++ src/tests/ExamTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import service.Exam;
+
+import static org.junit.Assert.*;
+
+public class ExamTest {
+
+    Exam exam;
+
+   @Before
+    public void  init() {
+       exam = new Exam("name", 1, "lu");
+    }
+
+    @Test
+    public void setName() {
+       exam.setName("newName");
+       assertTrue("Test setName", exam.getName().equals("newName"));
+    }
+
+    @Test
+    public void setId() {
+        exam.setId(2);
+        assertTrue("Test setId", exam.getId() == (2));
+    }
+
+    @Test
+    public void setLu() {
+        exam.setLu("newLu");
+        assertTrue("Test setLu", exam.getLu().equals("newLu"));
+    }
+
+    @Test
+    public void toString1(){
+       String name = exam.toString();
+
+       assertTrue("Test toString", name.equals(exam.getName()));
+    }
+
+}
---
+++ src/tests/LearningApplicationTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import service.LearningApplication;
+
+import static org.junit.Assert.*;
+
+
+public class LearningApplicationTest {
+    LearningApplication learningApplication;
+    @Before
+    public void init(){
+        learningApplication = new LearningApplication(1, "name", "description", 3, 2);
+
+    }
+
+    @Test
+    public void getApprovedFlag() {
+        assertTrue("Test getApprovedFlag", learningApplication.getApprovedFlag() == 3);
+    }
+
+    @Test
+    public void setApprovedFlag() {
+        learningApplication.setApprovedFlag(5);
+        assertTrue("Test setApprovedFlag", learningApplication.getApprovedFlag() == 5);
+    }
+
+    @Test
+    public void getCreatedBy() {
+        assertTrue("Test getCreatedBy", learningApplication.getCreatedBy() == 2);
+    }
+
+    @Test
+    public void setCreatedBy() {
+        learningApplication.setCreatedBy(26);
+        assertTrue("Test setCreatedBy", learningApplication.getCreatedBy() == 26);
+    }
+
+    @Test
+    public void toString1() {
+
+        assertTrue("Test toString", learningApplication.getName().equals("name"));
+    }
+}
